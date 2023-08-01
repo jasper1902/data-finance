bb0b865c022028f3f76d6f8052baf8d92908b8da
Jasper2002 <earth1887@gmail.com>
2023-08-01 21:19:00 +0700

    commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Analytics from "./components/Analytics"
+import Cards from "./components/Cards"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Nav from "./components/Nav"
+import Newsletter from "./components/Newsletter"
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { HiMenu } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+const Nav = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+    return (
+        <>
+            <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+                <ul className='md:flex gap-4 hidden'>
+                    <li className='cursor-pointer'>Home</li>
+                    <li className='cursor-pointer'>Company</li>
+                    <li className='cursor-pointer'>Resources</li>
+                    <li className='cursor-pointer'>About</li>
+                    <li className='cursor-pointer'>Contact</li>
+                </ul>
+
+                <div onClick={handleNav} className='cursor-pointer block md:hidden' draggable>
+                    {!nav ? <HiMenu size={20} /> : <AiOutlineClose size={20} />}
+                </div>
+
+                <div className={`${!nav ? "fixed left-0 top-0 w-[30%] border-r h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"} md:hidden`}>
+                    <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+                    <ul className='uppercase p-4'>
+                        <li className='cursor-pointer p-4 border-b border-gray-600'>Home</li>
+                        <li className='cursor-pointer p-4 border-b border-gray-600'>Company</li>
+                        <li className='cursor-pointer p-4 border-b border-gray-600'>Resources</li>
+                        <li className='cursor-pointer p-4 border-b border-gray-600'>About</li>
+                        <li className='cursor-pointer p-4 border-b border-gray-600'>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Nav
